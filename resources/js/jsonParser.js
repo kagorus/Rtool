273796842c84858cfdfaf6fa3f3e63198bb99466
Kagorus <nathan@kagorus.net>
2023-01-17 22:13:10 +0000

    Loop

--- resources/js/jsonParser.js
+++ resources/js/jsonParser.js
@@ -1,8 +1,8 @@
-var Directories = [];
-var Files = [];
-var IgnoreDir =["..",".",".git"]
+const Directories = [];
+const Files = [];
+const IgnoreDir =["..",".",".git"]
 //Leave me Blank After Writing Directory Chooser
-var WorkingDir = "/home/kagorus/RogueTech/";
+const WorkingDir = "/home/kagorus/RogueTech/";
 
 let readDir = async (dir) => {
     let entries = '';
@@ -17,14 +17,11 @@ let readDir = async (dir) => {
     entries.forEach(current => {
         //console.log(current);
         if(current.type == "FILE" && current.entry.substr(-5) == ".json"){
-            //console.log(current.entry);
-            // Files.push([current.entry]);
-            
+            //console.log(current.entry);            
             Files.push({"FileName":current.entry,"Directory":dir});
         }
         else if(current.type == "DIRECTORY"){
             //console.log(current.entry);
-            // Directories.push(current.entry);
             if(!IgnoreDir.includes(current.entry)){
                 Directories.push({"To Scan":current.entry,"Directory":dir});
             }
@@ -33,10 +30,21 @@ let readDir = async (dir) => {
     });
     console.log("Jsons Detected: " + Files.length + "  " + JSON.stringify(Files,null,4));
     console.log("Folders Detected: " + Directories.length + "  " + JSON.stringify(Directories,null,4));
+    //Checks to see if any directories need scanning
+    checkDirectories();
   }
   readDir(WorkingDir);
 
 
+function checkDirectories(){
+    let current = Directories[0].Directory;
+        Directories.shift();
+    if(Directories.length != 0){
+        
+        console.log("Folders Detected: " + Directories.length + "  " + JSON.stringify(Directories,null,4));
+        readDir(current);
+    }
+}
 
 // let memoryInfo = await Neutralino.computer.getMemoryInfo();
 // console.log(`RAM size: ${memoryInfo.physical.total}B`);
